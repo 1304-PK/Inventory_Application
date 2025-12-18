e04b08156959c250620d2be4c6d4e6d6c69b4b7d
PK-1304 <pushkark1304@gmail.com>
2025-12-18 19:26:19 +0530

    query the database for all products

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
+const db = require('./db')
 
 app.use(express.static('public'))
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname+'/views/index.html')
+})
+
+app.get("/products", async (req, res)=>{
+    try{
+        const [rows] = await db.query('SELECT * FROM products')
+        res.render('products.ejs', {products_list:rows})
+    } catch(err){
+        res.status(500).send('Error')
+    }
+})
+
+app.get("/categories", (req, res)=>{
+    res.send()
 })
 
 app.listen(process.env.PORT, (err)=>{
